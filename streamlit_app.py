9ef386fe147ee12ce5a589e022fb95b283004390
Sho Shimauchi <sho.shimauchi@gmail.com>
2023-05-29 00:07:42 +0000

    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,9 @@ def rgb_to_hex(r, g, b):
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-    red = st.slide("color RED", 0, 255, 0)
-    green = st.slide("color RED", 0, 255, 104)
-    blue = st.slide("color RED", 0, 255, 201)
+    red = st.slider("color RED", 0, 255, 0)
+    green = st.slider("color RED", 0, 255, 104)
+    blue = st.slider("color RED", 0, 255, 201)
 
     Point = namedtuple('Point', 'x y')
     data = []
